02dd561afe07accfda0848dd99e12f6963c97992
gabriela prodan <gabrielahtml@proton.me>
2024-02-28 18:18:43 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./components/style.css";
+import Meal from "./components/Meal";
+
+function App() {
+  return (
+    <div className="App">
+      <Meal />
+    </div>
+  );
+}
+
+export default App;
